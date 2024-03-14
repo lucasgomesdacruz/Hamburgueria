2757a7a7e90463ae51f7615ef800fb1086bd8aad
Lucas Gomes <lucaass1997rp@gmail.com>
2024-03-13 21:30:51 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'sans': ['Poppins', 'sans-serif']
+      },
+      backgroundImage:{
+        "home": "url('/assets/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
